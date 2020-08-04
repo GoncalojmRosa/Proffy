55d1b34199fa29bc8b35a217a4a50e65e22ae57a
Aspi-code <62306545+Aspi-code@users.noreply.github.com>
2020-08-04 15:52:42 +0100

    Add files via upload

---
+++ server/src/routes.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import ClassesController from './controllers/ClassesController';
+import ConnectionController from './controllers/ConnectionController';
+
+const routes = express.Router();
+
+const classesController = new ClassesController();
+const connectionsController = new ConnectionController();
+
+routes.get('/classes', classesController.index);
+routes.post('/classes', classesController.create);
+
+routes.get('/connections', connectionsController.index);
+routes.post('/connections', connectionsController.create);
+
+export default routes;
